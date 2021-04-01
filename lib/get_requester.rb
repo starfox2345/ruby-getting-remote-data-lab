c8e25a923b64a7cabfd2b3b6051ac91191a6ee0c
yatsun <yatsunlam25@gmail.com>
2021-03-31 23:45:17 -0400

    commit tree d81a9d5d3f2fe1fc27346182e3c2d574e745d539

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,7 +3,6 @@ require 'json'
 require 'open-uri'
 require 'net/http'
 
-
 class GetRequester
     def initialize(url)
         @URL = url
@@ -16,6 +15,50 @@ class GetRequester
     end
 
     def parse_json
-        response_hash = JSON.parse(self.get_response_body)
+        hash = JSON.parse(self.get_response_body)
     end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class GetRequester
+#     def initialize(url)
+#         @URL = url
+#     end
+
+#     def get_response_body
+#         uri = URI.parse(@URL)
+#         response = Net::HTTP.get_response(uri)
+#         response.body
+#     end
+
+#     def parse_json
+#         response_hash = JSON.parse(self.get_response_body)
+#     end
+# end
